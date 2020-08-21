9c7026dd9d30d22eca590219cb4c0155846055eb
Bat <BataevTechno@yandex.ru>
2020-08-21 16:32:40 +0900

    Add project files.

---
+++ Shutdown_Weekend/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Shutdown_Weekend
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime dayOfWeek = DateTime.Now;
+            int p = Convert.ToInt32(dayOfWeek.DayOfWeek);
+            if (p == 6 || p == 0)
+            {
+                Process shutdown = new Process();
+                shutdown.StartInfo.FileName = "cmd.exe";
+                shutdown.StartInfo.Arguments = "/c shutdown -s -t 60";
+                shutdown.Start();
+            }
+        }
+    }
+}
