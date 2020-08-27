259cf97307e35b8ba438b869efbf36b80ccd625d
Bat <BataevTechno@yandex.ru>
2020-08-27 16:15:45 +0900

    5 minutes delay added

--- Shutdown_Weekend/Program.cs
+++ Shutdown_Weekend/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Timers;
 
 namespace Shutdown_Weekend
 {
     class Program
     {
+        static void Shutdown(object source, ElapsedEventArgs e)
+        {
+            Process shutdown = new Process();
+            shutdown.StartInfo.FileName = "cmd.exe";
+            shutdown.StartInfo.Arguments = "/c shutdown -s -t 5";
+            shutdown.Start();
+        }
         static void Main()
         {
             DateTime dayOfWeek = DateTime.Now;
             int p = Convert.ToInt32(dayOfWeek.DayOfWeek);
             if (p == 6 || p == 0)
             {
-                Process shutdown = new Process();
-                shutdown.StartInfo.FileName = "cmd.exe";
-                shutdown.StartInfo.Arguments = "/c shutdown -s -t 60";
-                shutdown.Start();
+                const int interval = 300000;
+                Timer tmr = new Timer();
+                tmr.Elapsed += new ElapsedEventHandler(Shutdown);
+                tmr.Interval = interval;
+                tmr.Enabled = true;
+                Console.WriteLine("Нажмите Enter для выхода");
+                Console.ReadKey();
             }
         }
     }
